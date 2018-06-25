0c67e3871f89de1c00244d7c38e70efe2946dd5d
Blake Howe <blakehowe@hotmail.com>
2018-06-25 16:14:54 +1000

    security implementation

--- src/main/java/zuulgatewaydemo/demo/GatewayApplication.java
+++ src/main/java/zuulgatewaydemo/demo/GatewayApplication.java
@@ -1,9 +1,14 @@
 package zuulgatewaydemo.demo;
 
+import feign.RequestInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,5 +17,10 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RequestInterceptor getUserFeignClientInterceptor() {
+        return new UserFeignClientInterceptor();
     }
 }
